6b4259eb65762ba0ed24afc05f52f16f26e3c2ff
SK <sun-kyu.kim@aya.yale.edu>
2024-12-06 11:54:00 -0500

    c

--- src/server.ts
+++ src/server.ts
@@ -71,137 +71,96 @@ export class FeedGenerator {
     await events.once(this.server, 'listening')
     const prisma = new PrismaClient()
 
-    async function myScheduledFunction() {
+    async function createRanks() {
       const mostRecentPage = await prisma.pageRank.findFirst({
         orderBy: {
           createdAt: 'desc',
         },
       });
       let batch = mostRecentPage?.batch
-      if(!batch){
+      if (!batch) {
         batch = 1
+      } else {
+        batch++
       }
-//       let res: Array<{ id: number, linkId: number, likesCount: number, repostsCount: number, repliesCount: number, url: string, title: string, description: string, postCreatedAt: Date, likes: number, reposts: number, replies: number, date: string, score: number }> = await prisma.$queryRaw`WITH 
-// RecentPosts AS (
-//     SELECT did, createdAt
-//     FROM Post
-//     WHERE createdAt >= NOW() - INTERVAL 5 MINUTE
-// ),
-// LikeCounts AS (
-//     SELECT postId, COUNT(*) AS likes
-//     FROM \`Like\`
-//     WHERE createdAt >= NOW() - INTERVAL 5 MINUTE
-//     GROUP BY postId
-//     HAVING COUNT(*) >= 5
-// ),
-// RepostCounts AS (
-//     SELECT postId, COUNT(*) AS reposts
-//     FROM Repost
-//     WHERE createdAt >= NOW() - INTERVAL 5 MINUTE
-//     GROUP BY postId
-//     HAVING COUNT(*) >= 2
-// ),
-// ReplyCounts AS (
-//     SELECT postId, COUNT(*) AS replies
-//     FROM Reply
-//     WHERE createdAt >= NOW() - INTERVAL 5 MINUTE
-//     GROUP BY postId
-//     HAVING COUNT(*) >= 1
-// )
-// SELECT 
-//     l.*, 
-//     MIN(p.createdAt) AS postCreatedAt,
-//     SUM(COALESCE(lc.likes, 0)) AS likesCount,
-//     SUM(COALESCE(rc.reposts, 0)) AS repostsCount,
-//     SUM(COALESCE(rp.replies, 0)) AS repliesCount
-// FROM Link l
-// JOIN Post p ON l.id = p.linkId
-// LEFT JOIN LikeCounts lc ON p.did = lc.postId
-// LEFT JOIN RepostCounts rc ON p.did = rc.postId
-// LEFT JOIN ReplyCounts rp ON p.did = rp.postId
-// WHERE 
-//     p.did IN (SELECT did FROM RecentPosts)
-//     OR p.did IN (SELECT postId FROM LikeCounts)
-//     OR p.did IN (SELECT postId FROM RepostCounts)
-//     OR p.did IN (SELECT postId FROM ReplyCounts)
-// GROUP BY l.id;`
 
-let res: Array<{ id: number, linkId: number, url: string, title: string, description: string, postCreatedAt: Date, postId: number, score: number }> = await prisma.$queryRaw`SELECT 
-    l.id AS linkId, 
-    l.url AS url, 
-    l.title AS title,
-    l.description AS description,
-    p.id AS postId,
-    p.createdAt,
-    (SELECT COUNT(*) FROM Post p2 WHERE p2.linkId = l.id) AS postCount,
-    (
-        (COUNT(likes.id) + COUNT(reposts.id) * 2 + COUNT(replies.id) * 3)
-    ) AS score
+      let res: Array<{ id: number, linkId: number, url: string, title: string, description: string, postCreatedAt: Date, postId: number, score: number }> = await prisma.$queryRaw`SELECT 
+l.id AS linkId, 
+l.url AS url, 
+l.title AS title,
+l.description AS description,
+p.id AS postId,
+p.createdAt,
+(COUNT(p.id) * 2 + COUNT(likes.id) * 1 + COUNT(reposts.id) * 3 + COUNT(replies.id) * 4) AS score,
+(
+    (COUNT(p.id) * 200 + COUNT(likes.id) * 100 + COUNT(reposts.id) * 300 + COUNT(replies.id) * 400)
+    / (POW(LEAST(TIMESTAMPDIFF(MINUTE, p.createdAt, NOW()), 10080) + 300, 2))
+) AS ranking
 FROM Link l
 JOIN Post p ON l.id = p.linkId
 LEFT JOIN \`Like\` likes ON likes.postId = p.did AND likes.createdAt >= NOW() - INTERVAL 1 DAY
 LEFT JOIN Repost reposts ON reposts.postId = p.did AND reposts.createdAt >= NOW() - INTERVAL 1 DAY
 LEFT JOIN Reply replies ON replies.postId = p.did AND replies.createdAt >= NOW() - INTERVAL 1 DAY
 WHERE 
-    p.createdAt >= NOW() - INTERVAL 1 DAY
-    OR (
-        (SELECT COUNT(*) FROM \`Like\` WHERE postId = p.did AND createdAt >= NOW() - INTERVAL 1 DAY) >= 1
-    )
-    OR (
-        (SELECT COUNT(*) FROM Repost WHERE postId = p.did AND createdAt >= NOW() - INTERVAL 1 DAY) >= 1
-    )
-    OR (
-        (SELECT COUNT(*) FROM Reply WHERE postId = p.did AND createdAt >= NOW() - INTERVAL 1 DAY) >= 1
-    )
+p.createdAt >= NOW() - INTERVAL 1 DAY
+OR (
+    (SELECT COUNT(*) FROM \`Like\` WHERE postId = p.did AND createdAt >= NOW() - INTERVAL 1 DAY) >= 1
+)
+OR (
+    (SELECT COUNT(*) FROM Repost WHERE postId = p.did AND createdAt >= NOW() - INTERVAL 1 DAY) >= 1
+)
+OR (
+    (SELECT COUNT(*) FROM Reply WHERE postId = p.did AND createdAt >= NOW() - INTERVAL 1 DAY) >= 1
+)
 GROUP BY p.id
-ORDER BY score DESC
-LIMIT 25;`
+ORDER BY ranking DESC
+LIMIT 25;`;
 
-for(let row of res){
-  console.log(res)
-}
-
-
-    // const pages: Array<{
-    //   linkId: number,
-    //   url: string,
-    //   title: string,
-    //   description: string,
-    //   likes: number,
-    //   reposts: number,
-    //   replies: number,
-    //   createdAt: Date,
-    //   score: number
-    // }> = []
-    //   for (let row of res) {
-    //     let obj: {
-    //       linkId: number,
-    //       url: string,
-    //       title: string,
-    //       description: string,
-    //       likes: number,
-    //       reposts: number,
-    //       replies: number,
-    //       createdAt: Date,
-    //       score: number
-    //     } = {
-    //       linkId: row.id,
-    //       url: row.url,
-    //       title: row.title,
-    //       description: row.description,
-    //       likes: Number(row.likesCount),
-    //       reposts: Number(row.repostsCount),
-    //       replies: Number(row.repliesCount),
-    //       createdAt: row.postCreatedAt,
-    //       score: (Math.log(row.likesCount + 1) / Math.log(2)) + (Math.log(row.repostsCount + 1) / Math.log(1.5)) + (Math.log(row.repliesCount + 1) / Math.log(1.1))
-    //     }
-    //     pages.push(obj)
-    //   }
-    //   await prisma.pageRank.createMany({ data: pages })
+      const pages: Array<{
+        linkId: number,
+        url: string,
+        title: string,
+        description: string,
+        createdAt: Date,
+        score: number
+      }> = []
+      let rank = 1
+      for (let row of res) {
+        let obj: {
+          linkId: number,
+          url: string,
+          title: string,
+          description: string,
+          createdAt: Date,
+          score: number,
+          batch: number,
+          rank: number
+        } = {
+          linkId: row.id,
+          url: row.url,
+          title: row.title,
+          description: row.description,
+          createdAt: row.postCreatedAt,
+          score: row.score,
+          batch,
+          rank
+        }
+        pages.push(obj)
+        rank++
+      }
+      await prisma.pageRank.createMany({ data: pages })
     }
 
-    // setInterval(myScheduledFunction, 300000);
-    myScheduledFunction()
+    async function deleteOldRows() {
+      await prisma.$executeRaw`DELETE FROM \`Like\` WHERE createdAt < NOW() - INTERVAL 1 DAY;`
+      await prisma.$executeRaw`DELETE FROM \`Repost\` WHERE createdAt < NOW() - INTERVAL 1 DAY;`
+      await prisma.$executeRaw`DELETE FROM \`Reply\` WHERE createdAt < NOW() - INTERVAL 1 DAY;`
+    }
+
+    // Create new pageRanks every 5 minutes
+    setInterval(() => createRanks(), 300000)
+    // Delete old likes, reposts, and replies every day
+    setInterval(() => deleteOldRows(), 86400000)
     return this.server
   }
 }
